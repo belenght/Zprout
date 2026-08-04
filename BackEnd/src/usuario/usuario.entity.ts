4e498c52bef9a75e9b739a08a14b1e68e4b6827e
Fede_Mest <fede.mestre01@gmail.com>
2026-08-03 21:28:25 -0300

    Agregar Interface Test

---
+++ BackEnd/src/usuario/usuario.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+
+@Entity()
+export class User {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+
+  // unique: true asegura que no haya dos usuarios con el mismo correo en la base de datos
+  @Property({ unique: true })
+  email!: string;
+
+  @Property()
+  password!: string;
+
+  @Property()
+  createdAt: Date = new Date();
+}
